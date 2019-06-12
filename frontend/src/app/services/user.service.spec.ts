67ca6b406f51eaa615f8e18e488d1a512af23506
Eberhard Beilharz <eb1@sil.org>
2019-06-12 18:41:55 +0200

    Fix always passing frontend tests (#16)

--- frontend/src/app/services/user.service.spec.ts
+++ frontend/src/app/services/user.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { StorageServiceModule } from 'angular-webstorage-service';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, StorageServiceModule],
+    });
+  });
 
   it('should be created', () => {
     const service: UserService = TestBed.get(UserService);
