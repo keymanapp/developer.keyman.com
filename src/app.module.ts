39eb10ec1b7fe086d88ffdb5f5bbefaeac1f61b1
Eberhard Beilharz <ermshiperete@users.noreply.github.com>
2019-09-06 12:52:29 +0200

    Use MemoryStore package (#25)
    The MemoryStore implementation included in express-session is not
    suitable for production and outputs a warning. Instead we use the
    MemoryStore package. However, at least when running unit tests
    this doesn't properly shut down, therefore we don't use it when
    running tests. We'll have to see if this is suitable on the
    production server.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,17 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { CookieParserMiddleware } from '@nest-middlewares/cookie-parser';
 import { HelmetMiddleware } from '@nest-middlewares/helmet';
 import { ExpressSessionMiddleware } from '@nest-middlewares/express-session';
+import * as expressSession from 'express-session';
+
 import { AngularModule } from './angular/angular.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from './config/config.module';
 import { ConfigService } from './config/config.service';
 import { ProjectsModule } from './projects/projects.module';
 import { GithubModule } from './github/github.module';
+
+import memoryStoreModule = require('memorystore');
+const MemoryStore = memoryStoreModule(expressSession);
 
 @Module({
   imports: [
@@ -53,12 +58,24 @@ export class AppModule implements NestModule {
       cookie.secure = false;
     }
 
-    ExpressSessionMiddleware.configure({
+    const sessionOpts: expressSession.SessionOptions = {
       secret: this.configService.sessionSecret,
       cookie,
       saveUninitialized: false,
       resave: false,
-    });
+    };
+
+    if (process.env.NODE_ENV !== 'test') {
+      // It seems that MemoryStore has a bug (https://github.com/roccomuso/memorystore/issues/11)
+      // so that it doesn't properly shut down when unit tests end
+      sessionOpts.store = new MemoryStore({
+        checkPeriod: 24 * 60 * 60 * 1000, // 24 hrs * 60 min * 60 sec * 1000 ms
+      });
+    } else {
+      cookie.secure = false;
+    }
+
+    ExpressSessionMiddleware.configure(sessionOpts);
 
     consumer.apply(CookieParserMiddleware).forRoutes('/');
     consumer.apply(HelmetMiddleware).forRoutes('/');
