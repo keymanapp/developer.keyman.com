b9e05a6448d627cc0d031c658305149f22ea0029
Eberhard Beilharz <eb1@sil.org>
2020-02-27 19:28:31 +0100

    chore: Migrate to eslint instead of tslint
    See https://github.com/palantir/tslint/issues/4534.

---
+++ .eslintrc.js
@@ -0,0 +1,106 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    '@typescript-eslint/tslint',
+  ],
+  rules: {
+    '@typescript-eslint/array-type': 'error',
+    '@typescript-eslint/explicit-member-accessibility': [
+      'off',
+      {
+        accessibility: 'explicit',
+      },
+    ],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/member-ordering': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/prefer-for-of': 'error',
+    '@typescript-eslint/prefer-function-type': 'error',
+    '@typescript-eslint/quotes': ['error', 'single'],
+    '@typescript-eslint/unified-signatures': 'error',
+    'arrow-parens': ['off', 'as-needed'],
+    camelcase: 'error',
+    complexity: 'off',
+    'constructor-super': 'error',
+    'dot-notation': 'error',
+    eqeqeq: ['error', 'smart'],
+    'guard-for-in': 'error',
+    'id-blacklist': [
+      'error',
+      'any',
+      'Number',
+      'number',
+      'String',
+      'string',
+      'Boolean',
+      'boolean',
+      'Undefined',
+      'undefined',
+    ],
+    'id-match': 'error',
+    'import/order': 'off',
+    'max-classes-per-file': ['error', 1],
+    'max-len': [
+      'error',
+      {
+        code: 150,
+      },
+    ],
+    'new-parens': 'error',
+    'no-bitwise': 'error',
+    'no-caller': 'error',
+    'no-cond-assign': 'error',
+    'no-console': 'error',
+    'no-debugger': 'error',
+    'no-empty': 'error',
+    'no-eval': 'error',
+    'no-fallthrough': 'off',
+    'no-invalid-this': 'off',
+    'no-new-wrappers': 'error',
+    'no-shadow': [
+      'error',
+      {
+        hoist: 'all',
+      },
+    ],
+    'no-throw-literal': 'error',
+    'no-trailing-spaces': 'error',
+    'no-undef-init': 'error',
+    'no-underscore-dangle': 'error',
+    'no-unsafe-finally': 'error',
+    'no-unused-expressions': 'error',
+    'no-unused-labels': 'error',
+    'object-shorthand': 'error',
+    'one-var': ['error', 'never'],
+    radix: 'error',
+    'spaced-comment': ['error', 'always', {
+      'line': {
+          'markers': ['/'],
+          'exceptions': ['-', '+']
+      },
+      'block': {
+          'markers': ['!'],
+          'exceptions': ['*'],
+          'balanced': true
+      }
+    }],
+    'use-isnan': 'error',
+    'valid-typeof': 'off',
+  },
+};
