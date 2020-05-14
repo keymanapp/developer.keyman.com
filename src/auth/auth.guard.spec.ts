85fef0e9ec053afcfae135b77bae4ae440d43836
Eberhard Beilharz <eb1@sil.org>
2020-05-14 19:21:53 +0200

    fix(backend): Fix tests

--- src/auth/auth.guard.spec.ts
+++ src/auth/auth.guard.spec.ts
@@ -1,6 +1,7 @@
-import { AuthGuard } from './auth.guard';
-import { ExecutionContext } from '@nestjs/common';
+import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { HttpArgumentsHost } from '@nestjs/common/interfaces';
+
+import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   it('should be defined', () => {
@@ -11,6 +12,7 @@ describe('AuthGuard', () => {
     const mockRequest = jest.fn<any, any[]>(() => ({
       url: '/api/foo',
       session: { login: 'foo' },
+      headers: { authorization: 'foo' },
     }));
     const Mock = jest.fn<ExecutionContext, any[]>(() => ({
       getType: jest.fn(),
@@ -35,6 +37,7 @@ describe('AuthGuard', () => {
     const mockRequest = jest.fn<any, any[]>(() => ({
       url: '/api/foo',
       session: { login: null },
+      headers: { authorization: 'foo' },
     }));
     const Mock = jest.fn<ExecutionContext, any[]>(() => ({
       getType: jest.fn(),
@@ -52,14 +55,14 @@ describe('AuthGuard', () => {
     }));
     const context = new Mock();
 
-    expect(new AuthGuard().canActivate(context)).toBeFalsy();
+    expect(() => new AuthGuard().canActivate(context)).toThrow(UnauthorizedException);
   });
 
-  it.each([['/api/auth/login'], ['/api/auth/user']])(
-    'should activate if in auth module (%s) and no login', (url) => {
+  it('should activate if in auth/login module and no login', () => {
     const mockRequest = jest.fn<any, any[]>(() => ({
-      url,
+      url: '/api/auth/login',
       session: { login: null },
+      headers: {},
     }));
     const Mock = jest.fn<ExecutionContext, any[]>(() => ({
       getType: jest.fn(),
@@ -80,11 +83,88 @@ describe('AuthGuard', () => {
     expect(new AuthGuard().canActivate(context)).toBeTruthy();
   });
 
-  it.each([['/api/auth/login'], ['/api/auth/user']])(
-    'should activate if in auth module (%s) and have login', (url) => {
+  it(
+    'should activate if in auth/login module and have login', () => {
       const mockRequest = jest.fn<any, any[]>(() => ({
-        url,
+        url: '/api/auth/login',
         session: { login: 'foo' },
+        headers: {},
+      }));
+      const Mock = jest.fn<ExecutionContext, any[]>(() => ({
+        getType: jest.fn(),
+        getClass: jest.fn(),
+        getHandler: jest.fn(),
+        getArgs: jest.fn(),
+        getArgByIndex: jest.fn(),
+        switchToRpc: jest.fn(),
+        switchToHttp: jest.fn<HttpArgumentsHost, any[]>(() => ({
+          getRequest: mockRequest,
+          getResponse: jest.fn(),
+          getNext: jest.fn(),
+        })),
+        switchToWs: jest.fn(),
+      }));
+      const context = new Mock();
+
+      expect(new AuthGuard().canActivate(context)).toBeTruthy();
+    },
+  );
+
+  it('should not activate if in auth/user module and no login', () => {
+    const mockRequest = jest.fn<any, any[]>(() => ({
+      url: '/api/auth/user',
+      session: { login: null },
+      headers: {},
+    }));
+    const Mock = jest.fn<ExecutionContext, any[]>(() => ({
+      getType: jest.fn(),
+      getClass: jest.fn(),
+      getHandler: jest.fn(),
+      getArgs: jest.fn(),
+      getArgByIndex: jest.fn(),
+      switchToRpc: jest.fn(),
+      switchToHttp: jest.fn<HttpArgumentsHost, any[]>(() => ({
+        getRequest: mockRequest,
+        getResponse: jest.fn(),
+        getNext: jest.fn(),
+      })),
+      switchToWs: jest.fn(),
+    }));
+    const context = new Mock();
+
+    expect(() => new AuthGuard().canActivate(context)).toThrow(UnauthorizedException);
+  });
+
+  it('should not activate if in auth/user module and have login but not authorization', () => {
+    const mockRequest = jest.fn<any, any[]>(() => ({
+      url: '/api/auth/user',
+      session: { login: 'foo' },
+      headers: {},
+    }));
+    const Mock = jest.fn<ExecutionContext, any[]>(() => ({
+      getType: jest.fn(),
+      getClass: jest.fn(),
+      getHandler: jest.fn(),
+      getArgs: jest.fn(),
+      getArgByIndex: jest.fn(),
+      switchToRpc: jest.fn(),
+      switchToHttp: jest.fn<HttpArgumentsHost, any[]>(() => ({
+        getRequest: mockRequest,
+        getResponse: jest.fn(),
+        getNext: jest.fn(),
+      })),
+      switchToWs: jest.fn(),
+    }));
+    const context = new Mock();
+
+    expect(() => new AuthGuard().canActivate(context)).toThrow(UnauthorizedException);
+  });
+
+  it('should activate if in auth/user module and have login and authorization', () => {
+      const mockRequest = jest.fn<any, any[]>(() => ({
+        url: '/api/auth/user',
+        session: { login: 'foo' },
+        headers: { authorization: 'foo' },
       }));
       const Mock = jest.fn<ExecutionContext, any[]>(() => ({
         getType: jest.fn(),
