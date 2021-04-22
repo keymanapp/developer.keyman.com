24480d23c4a58b8d9b17416a220de485ce190299
Eberhard Beilharz <eb1@sil.org>
2021-04-22 11:22:36 +0200

    chore: Remove duplicate property

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,6 @@ module.exports = {
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-return': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
-    '@typescript-eslint/no-shadow': ['error'],
     '@typescript-eslint/prefer-for-of': 'error',
     '@typescript-eslint/prefer-function-type': 'error',
     '@typescript-eslint/restrict-template-expressions': 'off',
